1592cd01867abcf79b8365f0e5fc186e77227e8d
fanhuan <fanfan9447@163.com>
2022-09-15 18:11:33 +0800

    xxx

---
+++ packages/btn/index.js
@@ -0,0 +1,5 @@
+console.log(111)
+
+export default function () {
+    console.log(111)
+}
---
+++ packages/btn/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="index">index</div>
+</template>
+
+<script>
+export default {
+  name: "index",
+  data() {
+    return {};
+  },
+  components: {},
+  created() {},
+  mounted() {},
+  methods: {},
+};
+</script>
+<style lang='less' scoped>
+</style>
